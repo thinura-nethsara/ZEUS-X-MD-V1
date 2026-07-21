f67f4b90d8b156dfc8031d4645a5d7f9a6650a4a
thinura-nethsara <thinuranethsara2009@gmail.com>
2026-07-21 15:36:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'UrZmWIrR#3ak5KUPbU-WglfLpO8DRqtVinHfIqpS9-SvU94wl6WM',
+SESSION_ID:'xrw2XRrY#WMMku_1dHiKAbEZc1L08i7FgVOg8dk647Yhug17o07E',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'false' : process.env. MV_BLOCK,    
